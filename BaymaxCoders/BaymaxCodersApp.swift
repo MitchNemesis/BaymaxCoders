cea02a05014fc5d4ded644de90c8b2b2a235196f
Miguel Torres <m.torres.k01@gmail.com>
2023-04-24 14:18:23 -0700

    Initial Commit

---
+++ BaymaxCoders/BaymaxCodersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaymaxCodersApp.swift
+//  BaymaxCoders
+//
+//  Created by CEDAM01 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BaymaxCodersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
